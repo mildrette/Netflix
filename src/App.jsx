282e51d2f740793b3fa00d06fcc5b903cea9500d
mildrette <mildredfonka1@gmail.com>
2025-07-09 11:30:27 +0100

    doing it agian

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,17 @@
-import React from "react";
-import { CssBaseline } from "@mui/material";
-import Header from "./components/Header";
-import MovieRow from "./components/MovieRows";
-import HeroBanner from "./components/HeroBanner";
+import React from 'react'
+import Header from './components/Header'
 
-const trendingPosters = [
-  "/1.jpeg",
-  "/2.jpeg",
-  "/3.jpeg",
-  "/4.jpeg",
-  "/5.jpeg",
-  "/6.jpeg",
-  "/11.jpeg",
-  "/12.jpeg",
-];
-
-const actionPosters = [
-  "/7.jpeg",
-  "/8.jpeg",
-  "/9.jpeg",
-  "/10.jpeg",
-  "/1.jpeg",
-  "/2.jpeg",
-  "/3.jpeg",
-  "/4.jpeg",
-];
-
-const romanticPosters = [
-  "/11.jpeg",
-  "/12.jpeg",
-  "/1.jpeg",
-  "/2.jpeg",
-  "/3.jpeg",
-  "/4.jpeg",
-  "/3.jpeg",
-  "/4.jpeg",
-];
-
+import './App.css'
 
 function App() {
+
   return (
     <>
-      <Header />
-      <HeroBanner /> 
-      <CssBaseline />
-      <MovieRow title="Trending Now" posters={trendingPosters} />
-      <MovieRow title="Action" posters={actionPosters} />
-      <MovieRow title="Romantic Comedy" posters={romanticPosters} />
+      <Header /> 
+      
+    
     </>
-  );
+  )
 }
 
-export default App;
+export default App
